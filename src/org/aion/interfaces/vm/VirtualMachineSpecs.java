5a9252e30fa8b8286b206416afbab83f4599b8b6
Alexandra Roatis <alexandra@nuco.io>
2019-03-18 15:09:25 -0400

    removed the VirtualMachineSpecs since it is an implementation detail

--- src/org/aion/interfaces/vm/VirtualMachineSpecs.java
+++
@@ -1,7 +0,0 @@
-package org.aion.interfaces.vm;
-
-public final class VirtualMachineSpecs {
-
-    public static final byte AVM_CREATE_CODE = 0x0f;
-    public static final byte FVM_CREATE_CODE = 0x01;
-}
