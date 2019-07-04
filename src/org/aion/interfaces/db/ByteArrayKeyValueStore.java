aea0e5248ae6864bd761e7c8eaff633529b3b3fe
aionick <nick@nuco.io>
2019-07-04 14:49:06 -0400

    Remove all types in interfaces/db

--- src/org/aion/interfaces/db/ByteArrayKeyValueStore.java
+++
@@ -1,3 +0,0 @@
-package org.aion.interfaces.db;
-
-public interface ByteArrayKeyValueStore extends KeyValueStore<byte[], byte[]> {}
--- src/org/aion/interfaces/db/Flushable.java
+++
@@ -1,5 +0,0 @@
-package org.aion.interfaces.db;
-
-public interface Flushable {
-    void flush();
-}
