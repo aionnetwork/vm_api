1debc04901d7683fa3811947cc86f096d269cbd2
aionick <nick@nuco.io>
2019-07-04 15:05:09 -0400

    Remove all types in interfaces/functional

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,6 @@ module aion.vm.api {
     exports org.aion.vm.api.interfaces;
     exports org.aion.vm.api.types;
     exports org.aion.interfaces;
-    exports org.aion.interfaces.functional;
     exports org.aion.interfaces.tx;
     exports org.aion.interfaces.vm;
     exports org.aion.util;
