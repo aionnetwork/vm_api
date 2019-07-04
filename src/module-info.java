e953ab75b9a4cce3f0b8410e6be062daea30d8d8
aionick <nick@nuco.io>
2019-07-04 16:14:21 -0400

    Remove DataWord

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,5 @@ module aion.vm.api {
     requires aion.types;
     exports org.aion.vm.api.types;
     exports org.aion.interfaces;
-    exports org.aion.interfaces.vm;
     exports org.aion.util;
 }
