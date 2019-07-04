3106411f276c0631d3760010e1581e1bc0671ea7
aionick <nick@nuco.io>
2019-07-04 16:01:32 -0400

    Removed all types in vm.api.interfaces and interfaces.tx

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,7 @@
 module aion.vm.api {
     requires aion.types;
-    exports org.aion.vm.api.interfaces;
     exports org.aion.vm.api.types;
     exports org.aion.interfaces;
-    exports org.aion.interfaces.tx;
     exports org.aion.interfaces.vm;
     exports org.aion.util;
 }
